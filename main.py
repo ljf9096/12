ba9c80f3a86d17938f0bf2e0fc78e3e6648345c0
磨砺 <ljf9096@163.com>
2025-09-19 22:58:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -397,8 +397,13 @@ about="关于本源(塔利班维护),"+about_video2
 all_lines_simple =  ["更新时间,#genre#"] + [version] +[about]+['\n'] +\
                     ["央视频道,#genre#"] + read_txt_to_array('专区/央视频道.txt') + sort_data(ys_dictionary,ys_lines) + ['\n'] + \
                     ["卫视频道,#genre#"] + read_txt_to_array('专区/卫视频道.txt') + sort_data(ws_dictionary,ws_lines) + ['\n'] + \
+                    ["港澳台,#genre#"] + read_txt_to_array('专区/港澳台.txt') + sort_data(gat_dictionary,gat_lines) + ['\n'] + \
+                    ["台湾台,#genre#"] + read_txt_to_array('专区/台湾台.txt') + sort_data(twt_dictionary,twt_lines) + ['\n'] + \
+                    ["电影频道,#genre#"] + sort_data(dy_dictionary,dy_lines) + ['\n'] + \
+                    ["电视剧频道,#genre#"] + sort_data(dsj_dictionary,dsj_lines) + ['\n'] + \
+                    ["综艺频道,#genre#"] + sort_data(zy_dictionary,zy_lines) + ['\n'] + \
                     ["NewTV,#genre#"] + sort_data(newtv_dictionary,newtv_lines) + ['\n'] + \
-                    
+                    ["解说频道,#genre#"] + sorted(js_lines)
                     
 # 合并所有对象中的行文本（去重，排序后拼接）
 all_lines =  all_lines_simple + ['\n'] + \
@@ -525,6 +530,7 @@ print(f"others.txt行数: {other_lines_hj} ")
 #备用1：http://tonkiang.us
 #备用2：https://www.zoomeye.hk,https://www.shodan.io,https://tv.cctv.com/live/
 #备用3：(BlackList检测对象)http,rtmp,p3p,rtp（rtsp，p2p）
+
 
 
 
